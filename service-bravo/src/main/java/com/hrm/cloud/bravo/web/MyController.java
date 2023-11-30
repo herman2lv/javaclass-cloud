0d91be8031d4c239604f0a9f2ec388fdd3075e24
herman2lv <herman2lv@gmail.com>
2023-11-30 04:55:17 +0300

    Minor config fixes

--- service-bravo/src/main/java/com/hrm/cloud/bravo/web/MyController.java
+++ service-bravo/src/main/java/com/hrm/cloud/bravo/web/MyController.java
@@ -6,11 +6,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/bravo")
 public class MyController {
 
     private final MyService myService;
